09870bb32bd4bde11b5917f3b9c340e6a0bda39c
taha-khiari <85294214+taha-khiari@users.noreply.github.com>
2024-04-15 07:50:43 +0000

    update 15/04/2024

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import React from 'react';
 import './App.css';
-//import Form from './Component/form';
+import Form from './Component/form';
 import Homepage from './Homepage';
-
-
-
+import Map from './Component/map';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+ 
 function App() {
-
-
-return (
-    <div className="App">
-      <Homepage/>   
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <Routes>
+                        <Route path="/form" element={<Form />} />
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/map" element={<Map />} />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
 }
-
-export default App;
+ export default App;
